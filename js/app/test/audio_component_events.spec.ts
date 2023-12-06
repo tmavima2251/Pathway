dc1580dd4e6d35a28304b6177a1d3fae2ca23a1e
freddyaboulton <alfonsoboulton@gmail.com>
2023-12-06 14:40:21 -0500

    Audio test

--- js/app/test/audio_component_events.spec.ts
+++ js/app/test/audio_component_events.spec.ts
@@ -82,7 +82,15 @@ test("Play, Pause, and stop events work correctly.", async ({ page }) => {
 		.getByLabel("Play", { exact: true })
 		.click();
 
-	await expect(page.getByLabel("# Output Play Events")).toHaveValue("1");
-	await expect(page.getByLabel("# Output Pause Events")).toHaveValue("1");
-	await expect(page.getByLabel("# Output Stop Events")).toHaveValue("1");
+	const event_triggered = async (label: string) => {
+		const value = await page.getByLabel(label).inputValue();
+		expect(Number(value)).toBeGreaterThan(0);
+	};
+
+	// toPass will retry the function until it passes or times out
+	// need this because the stop event is only triggered when the video is done playing
+	// hard to time otherwise
+	await expect(async () => event_triggered("# Output Play Events")).toPass();
+	await expect(async () => event_triggered("# Output Pause Events")).toPass();
+	await expect(async () => event_triggered("# Output Stop Events")).toPass();
 });
